6376baf3178a59479f98abef0278418804900dd4
konusa <u-kostya@mail.ru>
2023-10-26 21:33:29 +0300

    2лаб

--- Условия и циклы/Лабораторные работы/Продолжение финансового планирования/main.py
+++ Условия и циклы/Лабораторные работы/Продолжение финансового планирования/main.py
@@ -3,6 +3,13 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
+result = 0
+for i in range(months):
+    result -= salary - spend
+    spend *= 1 + increase
+    # result2 = result + spend
+    # money_capital = result2 + spend
+
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", ...)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(result))
