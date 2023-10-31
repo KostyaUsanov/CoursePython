761ed48ceb08c8ecb5d281e242cbe7b7d6d7249e
konusa <u-kostya@mail.ru>
2023-10-31 21:00:46 +0300

    sync

--- Условия и циклы/Самостоятельная работа/task1/main.py
+++ Условия и циклы/Самостоятельная работа/task1/main.py
@@ -1,7 +1,9 @@
 src = not False and True or False and not True
-
+#src = True or False and not True
 # TODO расписать упрощение выражения
 
-result = ...  # TODO подставить результат выражения
+result = True and True or False and False
+result = True or False
+result = True# TODO подставить результат выражения
 
 print(src == result)
--- Условия и циклы/Самостоятельная работа/task4/main.py
+++ Условия и циклы/Самостоятельная работа/task4/main.py
@@ -1,2 +1,7 @@
-# TODO завести счетчик для произведения
+first = 1
+last = 10
+for i in range(1, last + 1):
+    first *= i
+print(first)
+
 # TODO посчитать произведение чисел
