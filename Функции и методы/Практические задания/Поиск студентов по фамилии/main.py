05b67e76e28d51774100b6d90c342f59e2450b53
konusa <u-kostya@mail.ru>
2023-11-09 14:09:18 +0300

    sync

--- Функции и методы/Практические задания/Поиск студентов по фамилии/main.py
+++ Функции и методы/Практические задания/Поиск студентов по фамилии/main.py
@@ -1,4 +1,9 @@
 def exists_student_by_lastname(students, lastname):
+    for student in students:
+        if student['фамилия'] == lastname:
+            return True
+    return False
+
     ...  # TODO Напишите проверку студента с заданной фамилией
 
 
@@ -9,6 +14,6 @@ students_list = [
     {'имя': 'Елена', 'фамилия': 'Сидорова'},
 ]
 
-for find_lastname in ['Иванов', 'Пупкин']:
+for find_lastname in ['Иванов', 'Пупкин', 'Дупкин', 'Сидорова']:
     is_exists_lastname = exists_student_by_lastname(students_list, find_lastname)
     print(f"Есть ли в базе студент с фамилией {find_lastname}? {is_exists_lastname}")
